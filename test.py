9776a1d807c5fc8f081d1b0e6881eb617a30d10e
narmita <narmitagoutam@gmail.com>
2025-10-05 19:10:36 +0530

    conditional program

---
+++ test.py
@@ -0,0 +1,27 @@
+x=input("what is your favourite color:")
+y=input("enter your fav color:")
+z=input("what is ur fav color:")
+if(x=="red"):
+    print("WON 100rs")
+elif(x=="blue"):
+    print("won 50rs")
+elif(x=="green"):
+    print("won 10rs")
+elif(x=="black"):
+    print("better luck nxt time!!")
+if(y=="red"):
+    print("WON 100rs")
+elif(y=="blue"):
+    print("won 50rs")
+elif(y=="green"):
+    print("won 10rs")
+elif(y=="black"):
+    print("better luck nxt time!!")
+if(z=="red"):
+    print("WON 100rs")
+elif(z=="blue"):
+    print("won 50rs")
+elif(z=="green"):
+    print("won 10rs")
+elif(z=="black"):
+    print("better luck nxt time!!")
