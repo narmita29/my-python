538581022618b7e5dd962aaf944997d1421cb830
narmita <narmitagoutam@gmail.com>
2025-10-05 18:25:45 +0530

    if_else_program

---
+++ if_else.py
@@ -0,0 +1,14 @@
+#WAP GRADE STUDENT BASED ON MARKS
+marks=int(input("enter student marks"))
+if(marks>=95):
+     print("Excellent:A++")
+elif(marks<95 and marks>=85):
+     print("very good: A+")
+elif(marks<85 and marks>=80):
+     print("well done:A GRADE")
+elif(marks<80 and marks>=70):
+     print("well try:B GRADE")
+elif(marks<70 and marks>=60):
+     print("keep it up: PASS")
+else:
+     print("work hard!!!") 
