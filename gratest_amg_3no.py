c921b862736d56f74a56f23154f55867e14ca42d
narmita <narmitagoutam@gmail.com>
2025-10-06 22:15:00 +0530

    greatest among three numbers

---
+++ gratest_amg_3no.py
@@ -0,0 +1,13 @@
+#WAP TO FIND GREATEST AMONG THREE NUMBER;
+a=int(input("ENTER YOUR FIRST NUMBER:"))
+b=int(input("ENTER YOUR SECOND NUMBER:"))
+c=int(input("ENTER YOUR THIRD NUMBER:"))
+if(a>=b and a>=c):
+    print("A IS GREATEST AMONG ALL THREE NUMBER")
+elif(b>=a and b>=c):
+    print("B IS GREATEST AMONG THREE NUMBER")
+else:
+    print("C IS GREATEST AMONG THREE NUMBER")
+
+
+print("thank you!!!!")
